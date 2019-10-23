758ec55963ebf6daf8adc845b904f930426b5692
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-23 13:03:35 +0200

    improvement: authenticate

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-env
+echo "$PLUGIN_GOOGLE_CREDENTIALS" > /tmp/.credentials
+gcloud auth activate-service-account --key-file /tmp/.credentials
+rm /tmp/.credentials
 echo "$PLUGIN_COMMANDS"
+echo "$PLUGIN_CONFIG"
