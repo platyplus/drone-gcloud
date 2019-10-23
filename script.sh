ce7f9d8b5d11796b8cbedeea0d5d2fce928c59dd
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-23 14:59:00 +0200

    improvement: long test

--- script.sh
+++ script.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
 # * Authentication
-echo "$PLUGIN_GOOGLE_CREDENTIALS" | base64 -d > /tmp/.credentials
-cat /tmp/credentials
-gcloud auth activate-service-account --key-file /tmp/.credentials
-rm /tmp/.credentials
+echo "$PLUGIN_GOOGLE_CREDENTIALS" | base64 -d > .credentials
+cat .credentials
+gcloud auth activate-service-account --key-file .credentials
+rm .credentials
 
-echo "$PLUGIN_MIXED"
+echo "$PLUGIN_t1"
+echo "$PLUGIN_t2"
+echo "$PLUGIN_t3"
+echo "$PLUGIN_t4"
 # * Set configuration
 # TODO test if empty env variable
 for row in $(echo "${PLUGIN_CONFIG}" | jq -r '.[] | @base64'); do
     decoded=$(echo "$row" | base64 -d)
     param=$(echo ${decoded} | jq -r 'keys[0]')
     value=$(echo ${decoded} | jq -r '.[keys[0]]')
-    echo "gcloud config set $param $value"
+    gcloud config set $param $value
 done
 
 # * Set possible environment variables from gcloud calls
-echo "${PLUGIN_EXPORTS}"
-for row in $(echo "${PLUGIN_EXPORTS}" | jq -r '.[] | @base64'); do
-    decoded=$(echo "$row" | base64 -d)
-    param=$(echo ${decoded} | jq -r 'keys[0]')
-    value=$(echo ${decoded} | jq -r '.[keys[0]]')
-    export ${param}=$(gcloud ${value})
+echo "${PLUGIN_COMMANDS}"
+# for row in $(echo "${PLUGIN_EXPORTS}" | jq -r '.[] | @base64'); do
+#     decoded=$(echo "$row" | base64 -d)
+#     param=$(echo ${decoded} | jq -r 'keys[0]')
+#     value=$(echo ${decoded} | jq -r '.[keys[0]]')
+#     export ${param}=$(gcloud ${value})
+# done
+for row in $(echo "${PLUGIN_COMMANDS}" | jq -r '.[] | @base64'); do
+    decoded=$(echo "$row" | base64 --decode)
+    if echo "$decoded" | jq -e . >/dev/null 2>&1; then
+        echo "object"
+        param=$(echo ${decoded} | jq -r 'keys[0]')
+        value=$(echo ${decoded} | jq -r '.[keys[0]]')
+        export $param="$(gcloud $value)"
+    else
+        gcloud $decoded
+    fi
 done
 
-IFS=','
-read -ra ADDR <<< "$PLUGIN_COMMANDS"
-for i in "${ADDR[@]}"; do
-    gcloud "$i"
-done
-IFS=' '
+# IFS=','
+# read -ra ADDR <<< "$PLUGIN_COMMANDS"
+# for i in "${ADDR[@]}"; do
+#     gcloud "$i"
+# done
+# IFS=' '
