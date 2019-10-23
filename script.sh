82edf03ed364f26cafdc10c224f8efab3ffa9e94
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-23 14:33:31 +0200

    improvement: exports

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# * Authentication
 echo "$PLUGIN_GOOGLE_CREDENTIALS" | base64 -d > /tmp/.credentials
 cat /tmp/credentials
 gcloud auth activate-service-account --key-file /tmp/.credentials
 rm /tmp/.credentials
+
+echo "$PLUGIN_MIXED"
+# * Set configuration
+# TODO test if empty env variable
 for row in $(echo "${PLUGIN_CONFIG}" | jq -r '.[] | @base64'); do
     decoded=$(echo "$row" | base64 -d)
     param=$(echo ${decoded} | jq -r 'keys[0]')
@@ -10,7 +15,15 @@ for row in $(echo "${PLUGIN_CONFIG}" | jq -r '.[] | @base64'); do
     echo "gcloud config set $param $value"
 done
 
-echo "$PLUGIN_EXPORTS"
+# * Set possible environment variables from gcloud calls
+echo "${PLUGIN_EXPORTS}"
+for row in $(echo "${PLUGIN_EXPORTS}" | jq -r '.[] | @base64'); do
+    decoded=$(echo "$row" | base64 -d)
+    param=$(echo ${decoded} | jq -r 'keys[0]')
+    value=$(echo ${decoded} | jq -r '.[keys[0]]')
+    export ${param}=$(gcloud ${value})
+done
+
 IFS=','
 read -ra ADDR <<< "$PLUGIN_COMMANDS"
 for i in "${ADDR[@]}"; do
