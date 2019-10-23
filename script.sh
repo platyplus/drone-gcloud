0ebdb192e9a4e7f7871d7587f91ed635ed822c8d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-23 14:13:13 +0200

    improvement: set config and commands

--- script.sh
+++ script.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-echo "$PLUGIN_GOOGLE_CREDENTIALS" | base64 -D > /tmp/.credentials
+echo "$PLUGIN_GOOGLE_CREDENTIALS" | base64 -d > /tmp/.credentials
 cat /tmp/credentials
 gcloud auth activate-service-account --key-file /tmp/.credentials
 rm /tmp/.credentials
-echo "$PLUGIN_COMMANDS"
-echo "$PLUGIN_CONFIG"
+for row in $(echo "${PLUGIN_CONFIG}" | jq -r '.[] | @base64'); do
+    decoded=$(echo "$row" | base64 -d)
+    param=$(echo ${decoded} | jq -r 'keys[0]')
+    value=$(echo ${decoded} | jq -r '.[keys[0]]')
+    echo "gcloud config set $param $value"
+done
+
+echo "$PLUGIN_EXPORTS"
+IFS=','
+read -ra ADDR <<< "$PLUGIN_COMMANDS"
+for i in "${ADDR[@]}"; do
+    gcloud "$i"
+done
+IFS=' '
