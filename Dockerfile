d86234a731bb0ddc1cdf5d1d8011439c8b13844f
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-23 21:04:21 +0200

    improvement: first real nodejs attempt

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM google/cloud-sdk:alpine
 RUN apk add --no-cache nodejs gettext
-# RUN apk add --no-cache jq gettext
 RUN gcloud components install beta
-# ADD script.sh /bin/
 ADD index.js /opt/
-# RUN chmod +x /bin/script.sh
 ENTRYPOINT ["node", "/opt/index.js"]
--- index.js
+++ index.js
@@ -1,17 +1,88 @@
-const configString = process.env.PLUGIN_CONFIG
-const commandsString = process.env.PLUGIN_COMMANDS
-// * Convert the credentials
-const credentialsEncoded = process.env.PLUGIN_CREDENTIALS
-console.log(credentialsEncoded, configString, commandsString)
-const buffer = new Buffer(credentialsEncoded, 'base64')
-const credentials = buffer.toString('ascii')
-console.log(credentials)
-// TODO * Write the credentials into a temporaty file
-// TODO * authenticate
-// TODO * Remove the temporary file
-let config = JSON.parse(configString)
-console.log(config)
-if (typeof config === 'string') config = config.split(',')
-for (item of config) {
-  console.log(item)
+'use strict'
+const fs = require('fs')
+const child_process = require('child_process')
+const TEMP_CREDENTIALS_FILE = '.credentials'
+const exit = function(message, error = undefined) {
+  console.error(message)
+  if (error) console.error(error)
+  process.exit(1)
 }
+const credentialsEncoded = process.env.PLUGIN_CREDENTIALS
+if (!credentialsEncoded) {
+  exit('No credentials')
+}
+// * Store the credentials into a temporary file
+try {
+  fs.writeFileSync(TEMP_CREDENTIALS_FILE, credentialsEncoded, {
+    encoding: 'base64'
+  })
+} catch (error) {
+  exit('Invalid credentials.', error)
+}
+// * authenticate
+try {
+  const command = `gcloud auth activate-service-account --key-file ${TEMP_CREDENTIALS_FILE}`
+  console.log(`Run: ${command}`)
+  child_process.execSync(command)
+} catch (error) {
+  exit('Authentication failed. See the following error:', error)
+}
+// * Delete the temporaty credentials file
+try {
+  fs.unlinkSync(TEMP_CREDENTIALS_FILE)
+} catch (error) {
+  exit('Impossible to remove the temporary credentials file.', error)
+}
+
+// * Set the configuration, if it exists
+const configString = process.env.PLUGIN_CONFIG
+if (configString) {
+  try {
+    let config = JSON.parse(configString)
+    //   if (typeof config === 'string') config = config.split(',')
+    for (const item of config) {
+      if (typeof item === 'string') {
+        exit(`Invalid configuration param: ${item}`, error)
+      }
+      try {
+        const param = Object.keys(item)[0]
+        const value = item[param]
+        const command = `gcloud config set ${param} ${value}`
+        console.log(`Run: ${command}`)
+        child_process.execSync(command)
+      } catch (error) {
+        exit(`Error in the configuration at ${item}`, error)
+      }
+    }
+  } catch (error) {
+    exit('Error in loading the Google Cloud configuration.', error)
+  }
+}
+
+// * Run the commands
+const commandsString = process.env.PLUGIN_COMMANDS
+if (commandsString) {
+  try {
+    let commands = JSON.parse(commandsString)
+    if (typeof commands === 'string') commands = commands.split(',')
+    for (let command of commands) {
+      let destVariable
+      if (typeof command !== 'string') {
+        destVariable = Object.keys(command)[0]
+        command = command[destVariable]
+      }
+      console.log(`Run: gcloud ${command}`)
+      const result = child_process
+        .execSync(`gcloud ${command}`)
+        .toString()
+        .trim()
+      if (destVariable) process.env[destVariable] = result
+      console.log(result)
+    }
+  } catch (error) {
+    exit('Error in the Google Cloud commands.', error)
+  }
+} else {
+  console.warn('No commands found. Nothing done.')
+}
+// console.log(process.env)
